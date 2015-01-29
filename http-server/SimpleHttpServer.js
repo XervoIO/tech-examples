9922f94ff42d6cbb88a394b888c1b1dbed8aa38e
Charlie Key <ckey@paranoidferret.com>
2015-01-29 12:02:18 -0500

    Update SimpleHttpServer.js
    Fixed grammar lets -> let's

--- http-server/SimpleHttpServer.js
+++ http-server/SimpleHttpServer.js
@@ -1,7 +1,7 @@
-//Lets require/import the HTTP module
+//Let's require/import the HTTP module
 var http = require('http');
 
-//Lets define a port we want to listen to
+//Let's define a port we want to listen to
 const PORT=8080;
 
 //We need a function which handles requests and send response
@@ -12,7 +12,7 @@ function handleRequest(request, response){
 //Create a server
 var server = http.createServer(handleRequest);
 
-//Lets start our server
+//Let's start our server
 server.listen(PORT, function(){
   //Callback triggered when server is successfully listening. Hurray!
   console.log('Server listening on: http://localhost:%s', PORT);
